bb7e95ebf2e2fb060764953ee33b6218b9aa68f8
Nirakar <goswaminirakr65@gmail.com>
2024-08-28 17:50:30 +0530

    fsdfa

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
+// const nextConfig = {
+//   output: 'export',
 
-    reactStrictMode: true,
-    images: {
-      loader: "akamai",
-      path: "public/assets/images/*",
-    },  
-    rewrites: [
-      {
-        "source": "/static/:slug*/index.html",
-        "destination": "/public/static/:slug*/index.html"
-      }
-    ]
-};
+//     reactStrictMode: true,
+//     images: {
+//       loader: "akamai",
+//       path: "public/assets/images/*",
+//     },  
+//     rewrites: [
+//       {
+//         "source": "/static/:slug*/index.html",
+//         "destination": "/public/static/:slug*/index.html"
+//       }
+//     ]
+// };
 
-export default nextConfig;
+// export default nextConfig;
